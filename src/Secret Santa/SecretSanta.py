aa80e0cb7706cb129b5b57b7aa7b74704c0f82e6
whoami?? {root} <71566358+sudo-whoami0-root@users.noreply.github.com>
2025-12-29 16:17:18 -0600

    set up naming logic in secret santa

---
+++ src/Secret Santa/SecretSanta.py
@@ -0,0 +1,18 @@
+import random
+
+name = []
+
+numPlayers = int(input("Enter the number of players playing: "))
+namesInputted = 0
+
+while namesInputted != numPlayers:
+
+    inputName = input("Enter the player name: ")
+    name.append(inputName)
+    namesInputted += 1
+
+
+
+
+
+
